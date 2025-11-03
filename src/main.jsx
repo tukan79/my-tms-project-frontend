714a4ce48ff03331c17f1f7ddfa6ab434924f2c6
Krzysztof Stolarski <krzysztofstolarski@MacBook-Pro-Krzysztof.local>
2025-11-03 23:03:21 +0000

    poprawk30

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext'; // Zaimportuj AuthProvider
+import { AuthProvider } from './contexts/AuthContext';
+import ErrorBoundary from './components/ErrorBoundary.jsx'; // Import globalnego ErrorBoundary
 import App from './App.jsx';
 import './index.css'; // lub inny plik stylów
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 // ostatnia zmiana (30.05.2024, 13:14:12)
